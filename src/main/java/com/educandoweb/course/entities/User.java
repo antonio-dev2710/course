94ef59ce7a923d8e853eecab58edacdfc4d63635
Antonio Alves <antoniocunha9610@gmail.com>
2023-04-28 20:52:31 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -3,6 +3,14 @@ package com.educandoweb.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name ="tb_user")
 public class User implements Serializable {
 	/**
 	 * 
@@ -13,6 +21,8 @@ public class User implements Serializable {
 //primeiro passo: criar os atributos basicos
 //segundo passo: associações(nesse caso n tá usando)
 //terceiro passo: construtores
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
