5fbdb5d8f80eb85c82e963fe6a85db45c8d3f4af
Antonio Alves <antoniocunha9610@gmail.com>
2023-05-24 21:28:34 -0300

    OrderItem, associação many-to-many com atributos extras PART-2

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -2,7 +2,9 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -13,6 +15,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -34,9 +37,12 @@ public class Order implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+	
+	//relação associativa: relacao oneToMany com order em telação a class composta OrderIPK
+	@OneToMany(mappedBy="id.order")
+	private Set<OrderItem> items = new HashSet<>();
 
 	// criar os construtores com e sem argumentos
-
 	public Order() {
 
 	}
@@ -83,6 +89,10 @@ public class Order implements Serializable {
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+	
+	public Set<OrderItem> getItems(){
+		return items;
 	}
 
 	@Override
---
+++ src/main/java/com/educandoweb/course/repositeries/OrderItemRepository.java
@@ -0,0 +1,11 @@
+package com.educandoweb.course.repositeries;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+}
