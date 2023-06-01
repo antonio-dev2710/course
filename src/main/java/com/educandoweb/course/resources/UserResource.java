634a1dc37765e8451818b1018f0d4f79eec11bdc
Antonio Alves <antoniocunha9610@gmail.com>
2023-05-31 21:44:10 -0300

    Atualizando usuário

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -74,6 +74,7 @@ public class UserResource {
 	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
 		
 		obj = service.update(id, obj);
+		
 		return ResponseEntity.ok().body(obj);
 	}
 
