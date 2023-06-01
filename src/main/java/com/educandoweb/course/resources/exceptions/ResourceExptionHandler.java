acc5bbca1466f21d82d2e2a45680d5473ca26712
Antonio Alves <antoniocunha9610@gmail.com>
2023-06-01 15:09:01 -0300

    Tratamento de exceção delete

--- src/main/java/com/educandoweb/course/resources/exceptions/ResourceExptionHandler.java
+++ src/main/java/com/educandoweb/course/resources/exceptions/ResourceExptionHandler.java
@@ -7,19 +7,34 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.educandoweb.course.services.exceptions.DataBaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
+///@ExceptionHandler(ResourceNotFoundException.class) essa anotation indica que esse metodo vai intercipitar qualquer exessão desse tipo que foi lançada e vai fzr o tratamento
 
-//essa anotação vai interceptar as excessoes que ocorrerem
+//@ControllerAdvice: essa anotação vai interceptar as excessoes que ocorrerem
 //para que esse objeto possa executar um tramento
 @ControllerAdvice
 public class ResourceExptionHandler {
-	//essa anotation indica que esse metodo vai intercipitar qualquer exessão desse tipo que foi lançada e vai fzr o tratamento
+	
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
 		String errror = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		
+		StandardError err = new StandardError(Instant.now(), status.value(), errror, e.getMessage(), request.getRequestURI());
+		
+		//retornar uma resposta personalizada
+		
+		return ResponseEntity.status(status).body(err);
+		
+	}
+	
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<StandardError> database(DataBaseException e, HttpServletRequest request){
+		String errror = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		
 		StandardError err = new StandardError(Instant.now(), status.value(), errror, e.getMessage(), request.getRequestURI());
 		
